9c051062ec6cd75bedce8cdadd5cd58f932c8261
Jim Kroon <info@jimkroon.nl>
2020-06-20 21:05:26 +0200

    Add login flow sample

---
+++ examples/login_flow.js
@@ -0,0 +1,56 @@
+var XboxApiClient = require('../src/client.js'); // or require('xbox-webapi')
+var TokenStore = require('../src/tokenstore.js'); // or require('xbox-webapi/src/tokenstore.js');
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var token_store = TokenStore()
+var client = XboxApiClient(token_store)
+
+client.authenticate().then(function(user_info){
+    client.provider('titlehub').get_title('442736763').then(function(data){
+
+        console.log('data', user_info, data)
+        rl.close();
+
+    }).catch(function(error){
+        console.log('error', error)
+        rl.close();
+    })
+
+}).catch(function(error){
+    if(error.error == 'authentication.failed' || error.error == 'authentication.need_setup'){
+        console.log('You are not authenticated. Please login first')
+
+        startLoginFlow()
+    } else {
+        console.log('error', error)
+        rl.close();
+    }
+})
+
+function startLoginFlow(){
+    console.log('Open the following link in your browser:', client.auth_manager._generate_authorization_url())
+    console.log('Make sure to copy the address after the redirect and paste it in the field below.')
+
+    rl.question('Copy url from browser and paste here: ', (response) => {
+        // TODO: Log the answer in a database
+        var results = client.process_authentication_response(response)
+
+        // console.log(results.access_token, results.refresh_token)
+        var token_store = TokenStore()
+        token_store.set('access_token', results.access_token)
+        token_store.set('refresh_token', results.refresh_token)
+        token_store.delete('user_token')
+        token_store.delete('xsts_token')
+        token_store.save()
+
+        console.log('Done, restart app');
+
+        rl.close();
+    });
+}
