5bdcd503a7c3f84165d16747c77d408ff249c8df
Jim Kroon <info@jimkroon.nl>
2020-06-20 22:20:58 +0200

    Added get presence in batch Added get friendslist example

--- src/providers/userpresence.js
+++ src/providers/userpresence.js
@@ -3,10 +3,19 @@ const BaseProvider = require('./base.js')
 
 module.exports = function(client)
 {
-    var Provider = BaseProvider(client, 'https://userpresence.xboxlive.com')
+    var Provider = BaseProvider(client, 'https://userpresence.xboxlive.com', {'x-xbl-contract-version': '3', 'Accept': 'application/json'})
 
     Provider.get_current_presence = function(){
         return this.get('users/me')
+    }
+
+    Provider.get_presence_batch = function(xuid_array, online_only = false, presence_level){
+
+        return this.post('users/batch', {
+            "users": xuid_array,
+            "onlineOnly": online_only,
+            "level": 'all'
+        })
     }
 
     Provider.get_user_presence = function(xid){
