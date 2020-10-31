ace8b7f0d2211b42981abb97a3e3585053201999
Jim Kroon <info@jimkroon.nl>
2020-10-31 07:00:07 +0100

    Improved refresh flow Added changeme log

--- src/authentication.js
+++ src/authentication.js
@@ -92,14 +92,28 @@ module.exports = function(clientId, secret){
             return new Promise(function(resolve, reject) {
                 if(type == 'oauth'){
                     if(this._tokens.oauth.refresh_token){
-                        var oauth_expire = new Date(new Date(this._tokens.oauth.issued).getTime() + (this._tokens.oauth.expires_in*1000))
+                        var oauth_expire_oauth = new Date(new Date(this._tokens.oauth.issued).getTime() + (this._tokens.oauth.expires_in*1000))
                         
-                        if(new Date() > oauth_expire){
+                        if(new Date() > oauth_expire_oauth){
                             // Oauth token expired, refresh token
                             console.log('TODO: refresh token')
+
+                            this.refreshToken(this._tokens.oauth.refresh_token).then(function(token){
+                                this._tokens.oauth = token
+                                this.saveTokens()
+
+                                this.refreshTokens('user').then(function(){
+                                    resolve()
+                                }).catch(function(error){
+                                    reject(error)
+                                })
+
+                            }).catch(function(error){
+                                reject('Unable to refresh oauth access token. Reauthenticate again')
+                            })
                         } else {
                             // Token is still valid
-                            console.log('oauth token is valid, continue to get user token')
+                            // console.log('oauth token is valid, continue to get user token')
 
                             this.refreshTokens('user').then(function(){
                                 resolve()
@@ -114,14 +128,28 @@ module.exports = function(clientId, secret){
                 } else if(type == 'user'){
 
                     if(this._tokens.user.Token){
-                        var oauth_expire = new Date(this._tokens.user.NotAfter).getTime()
+                        var oauth_expire_user = new Date(this._tokens.user.NotAfter).getTime()
                         
-                        if(new Date() > oauth_expire){
+                        if(new Date() > oauth_expire_user){
                             // Oauth token expired, refresh user token
                             console.log('TODO: refresh user token')
+
+                            this.getUserToken(this._tokens.oauth.access_token).then(function(token){
+                                this._tokens.user = token
+                                // this.saveTokens()
+
+                                this.refreshTokens('xsts').then(function(){
+                                    resolve()
+                                }).catch(function(error){
+                                    reject(error)
+                                })
+
+                            }).catch(function(error){
+                                reject('Unable to refresh oauth access token. Reauthenticate again')
+                            })
                         } else {
                             // Token is still valid
-                            console.log('user token is valid')
+                            // console.log('user token is valid')
 
                             this.refreshTokens('xsts').then(function(){
                                 resolve()
@@ -147,7 +175,7 @@ module.exports = function(clientId, secret){
                     }
 
                 } else if(type == 'xsts'){
-                    console.log(this._tokens)
+                    // console.log(this._tokens)
 
                     if(this._tokens.xsts.Token){
                         var oauth_expire = new Date(this._tokens.xsts.NotAfter).getTime()
@@ -157,7 +185,7 @@ module.exports = function(clientId, secret){
                             console.log('TODO: refresh xsts token')
                         } else {
                             // Token is still valid
-                            console.log('xsts token is valid')
+                            // console.log('xsts token is valid')
 
                             this._user = {
                                 gamertag: this._tokens.xsts.DisplayClaims.xui[0].gtg,
@@ -173,7 +201,7 @@ module.exports = function(clientId, secret){
                         this.getXstsToken(this._tokens.user.Token).then(function(data){
                             // Got user token, continue with xsts
                             this._tokens.xsts = data
-                            console.log(this._tokens)
+                            // console.log(this._tokens)
 
                             this._user = {
                                 gamertag: data.DisplayClaims.xui[0].gtg,
@@ -189,50 +217,6 @@ module.exports = function(clientId, secret){
                         })
                     }
                 }
-            }.bind(this))
-        },
-
-        refreshTokens2: function(){
-            // @TODO: Refactor this part so we check the timestamps and improve the renewall flow so long running processes can renew tokens too
-            return new Promise(function(resolve, reject) {
-
-                if(this._tokens.user.Token === undefined){
-                    this.getUserToken(this._tokens.oauth.access_token).then(function(data){
-                        this._tokens.user = data
-
-
-
-                        
-                        if(this._tokens.xsts.Token === undefined){
-                            // console.log('GETTING XSTS TOKEN', this._tokens.user)
-                            this.getXstsToken(this._tokens.user.Token).then(function(data){
-                                this._tokens.xsts = data
-
-                                this._user = {
-                                    gamertag: data.DisplayClaims.xui[0].gtg,
-                                    xid: data.DisplayClaims.xui[0].xid,
-                                    uhs: data.DisplayClaims.xui[0].uhs
-                                    // @TODO: Check if we need more data?
-                                }
-
-                                resolve()
-        
-                            }.bind(this)).catch(function(error){
-                                reject(error)
-                            }.bind(this))
-                        } else {
-                            resolve()
-                        }
-
-
-
-
-                    }.bind(this)).catch(function(error){
-                        reject(error)
-                    }.bind(this))
-
-                }
-
             }.bind(this))
         },
 
@@ -262,7 +246,7 @@ module.exports = function(clientId, secret){
             }.bind(this))
         },
 
-        refreshToken: function(refreshToken, callback){
+        refreshToken: function(refreshToken){
             return new Promise(function(resolve, reject) {
                 const tokenParams = {
                     "client_id": this._clientId,
@@ -287,7 +271,7 @@ module.exports = function(clientId, secret){
             }.bind(this))
         },
 
-        getUserToken: function(accessToken, callback){
+        getUserToken: function(accessToken){
             return new Promise(function(resolve, reject) {
                 const tokenParams = {
                     "RelyingParty": "http://auth.xboxlive.com",
@@ -312,7 +296,7 @@ module.exports = function(clientId, secret){
             }.bind(this))
         },
 
-        getXstsToken: function(userToken, callback){
+        getXstsToken: function(userToken){
             return new Promise(function(resolve, reject) {
                 const tokenParams = {
                     "RelyingParty": "http://xboxlive.com",
