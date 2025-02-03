90c0f9c43ecf65f8e64ffd54fc511698e612d69e
Jim Kroon <info@jimkroon.nl>
2025-02-03 23:58:39 +0100

    Update docs

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">XAL-Node Documentation</h1>
+      <h1 className="mb-4 text-2xl font-bold">Xbox-Webapi-Node Documentation</h1>
       <p className="text-fd-muted-foreground">
         Check out{' '}
         <Link
