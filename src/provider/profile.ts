bb97d5bdcb8cdde6cbf3cd052ad4b05dc4b9960f
Jim Kroon <info@jimkroon.nl>
2025-04-25 18:11:49 +0200

    add getCurrentUser() function to profile provider

--- src/provider/profile.ts
+++ src/provider/profile.ts
@@ -9,6 +9,10 @@ export default class ProfileProvider extends BaseProvider {
         'x-xbl-contract-version': 3
     }
 
+    async getCurrentUser(): Promise<HttpResponse<ProfileResponse>> {
+        return (await this.get('/users/me/profile/settings?settings=GameDisplayName,GameDisplayPicRaw,Gamerscore,Gamertag'))
+    }
+
     async getUserProfile(xuid:string): Promise<HttpResponse<ProfileResponse>> {
         return (await this.get('/users/xuid('+xuid+')/profile/settings?settings=GameDisplayName,GameDisplayPicRaw,Gamerscore,Gamertag'))
     }
