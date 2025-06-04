ff42d9b182b925db3bc8803b90749e28dd9550cd
Jim Kroon <info@jimkroon.nl>
2025-06-04 21:41:10 +0200

    Update api-build.mjs

--- docs/api-build.mjs
+++ docs/api-build.mjs
@@ -1,8 +1,10 @@
-import { generateFiles } from 'fumadocs-typescript';
+import { generateFiles, createGenerator } from 'fumadocs-typescript';
 import * as path from 'node:path';
- 
-void generateFiles({
-  input: ['./content/docs/**/*.model.mdx'],
+
+const generator = createGenerator()
+
+void generateFiles(generator, {
+  input: ['./content/docs/**/*.model.mdxmodel'],
   // Rename x.model.mdx to x.mdx
   output: (file) =>
     path.resolve(
